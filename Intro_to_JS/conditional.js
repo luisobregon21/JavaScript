933ed32d4f6208d0bbc48cd5ff0d519c0b2dce51
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-01-30 13:36:06 -0400

    Message("Add new task")

--- Intro_to_JS/conditional.js
+++ Intro_to_JS/conditional.js
@@ -5,10 +5,11 @@ const usr = process.argv[2];
 const nica = "Luis";
 const age = 19;
 
-if (process.argv.length != 3) {
+if (process.argv.length !== 3) {
     console.log("Error found, please try again");
+    return;
 }
-if (usr == nica) {
+if (usr === nica) {
     console.log("Hello my name is: " + nica + "\nMy age is: " + age);
 } else {
     console.log(usr + " is not the user");
---
+++ Intro_to_JS/stars.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+let idx = 1;
+
+for (idx = 1; idx <= 3; idx++) {
+  console.log('*'.repeat(idx));
+}
