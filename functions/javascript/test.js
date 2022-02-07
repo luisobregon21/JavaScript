d720bf24224ab721be3eede156b79feb8d273cdd
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-02-06 20:23:39 -0400

    Testing import

---
+++ functions/javascript/test.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+$(document).ready(function() {
+    $("#title").html("Welcome to the end of times");
+});
+alert("Hey: this is a test for the import");
+console.log("Seems about right :)");
